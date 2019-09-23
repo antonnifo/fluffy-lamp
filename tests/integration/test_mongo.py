68da2d7821c98f3ddf9088d8398b2e7a81c42e52
antonnifo <antonnifo@live.com>
2019-09-23 14:00:48 +0300

    add test data in setUp method

--- tests/integration/test_mongo.py
+++ tests/integration/test_mongo.py
@@ -2,8 +2,6 @@ import os
 from unittest import TestCase
 
 from mongo_config import MongoConfig
-from mongo_test_data import (insert_many_data, insert_one_data, print_many_query_data,
-                  print_many_query_2_data, update_one_query, set_values, update_many_query, set_values_2)
 
 BAD_CONNECTION_URL = os.getenv('BAD_CONNECTION_URL')
 
@@ -11,6 +9,23 @@ class MongoTestCase(TestCase):
 
     def setUp(self):
         self.db = MongoConfig()
+        self.insert_many_data        =  [
+
+    {"name" : "antonnifo", "company" : "Icodeai"           },
+    {"name" : "chris"    , "company" : "Techbiz"           },
+    {"name" : "ashley"   , "company" : "Yusudi"            },
+    {"name" : "mwas"     , "company" : "pangani smokers"   },
+    {"name" : "kamau"    , "company" : "kamau traders"     },
+    {"name" : "otieno"   , "company" : "otis sugar"        },
+    {"name" : "antonnif" , "company" : "icodea"            }
+]
+        self.insert_one_data         = {"name" : "anton" , "company" : "icodea"}
+        self.print_many_query_data   = {'name': {'$regex': '^a'}}
+        self.print_many_query_2_data = {'company': 'Icodeai'}
+        self.update_one_query        = {"name": "antonnif"}
+        self.set_values              = {"$set": {"name": "alaska"}}
+        self.update_many_query       = {"name" : {"$regex" : "^a"}}
+        self.set_values_2            = {"$set": {"name": "pynifo"}} 
     
     def test_get_client(self):
         self.assertNotEqual(self.db.get_client(),
@@ -37,11 +52,11 @@ class MongoTestCase(TestCase):
                              "Unable to use collection")
 
     def test_insert_one(self):
-         self.assertNotEqual(self.db.insert_one('my_database', 'customers', insert_one_data),
+         self.assertNotEqual(self.db.insert_one('my_database', 'customers', self.insert_one_data),
                              "Unable to insert document")
 
     def test_insert_many(self):
-         self.assertNotEqual(self.db.insert_many('my_database', 'customers', insert_many_data),
+         self.assertNotEqual(self.db.insert_many('my_database', 'customers', self.insert_many_data),
                              "Unable to insert more documents")
 
     def test_find_one(self):
@@ -52,24 +67,24 @@ class MongoTestCase(TestCase):
         self.assertNotEqual(self.db.find_many('my_database', 'customers'),
                               "Unable to print documents")
 
-        self.assertNotEqual(self.db.find_many('my_database', 'customers',print_many_query_data),
+        self.assertNotEqual(self.db.find_many('my_database', 'customers',self.print_many_query_data),
                              "Unable to print documents")
 
-        self.assertNotEqual(self.db.find_many('my_database', 'customers',print_many_query_2_data),
+        self.assertNotEqual(self.db.find_many('my_database', 'customers',self.print_many_query_2_data),
                              "Unable to print documents")
 
     def test_update_one(self):
         self.assertNotEqual(self.db.update_one(
-            'my_database', 'customers', update_one_query, set_values), 'Unable to update document')
+            'my_database', 'customers', self.update_one_query, self.set_values), 'Unable to update document')
 
     def test_fail_update_one(self):
         self.assertEqual(self.db.update_one(
-            'my_database', 'customers', update_one_query, set_values, BAD_CONNECTION_URL), 'Unable to update document')
+            'my_database', 'customers', self.update_one_query, self.set_values, BAD_CONNECTION_URL), 'Unable to update document')
 
     def test_update_many(self):
         self.assertNotEqual(self.db.update_many(
-            'my_database', 'customers', update_many_query, set_values_2), 'Unable to update the documents')
+            'my_database', 'customers', self.update_many_query, self.set_values_2), 'Unable to update the documents')
 
     def test_fail_update_many(self):
         self.assertEqual(self.db.update_many(
-            'my_database', 'customers', update_many_query, set_values_2, BAD_CONNECTION_URL), 'Unable to update the documents')
+            'my_database', 'customers', self.update_many_query, self.set_values_2, BAD_CONNECTION_URL), 'Unable to update the documents')
